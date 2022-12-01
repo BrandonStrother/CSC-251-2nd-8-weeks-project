f51bdd386f6361a27f7840d99daae9293e19c639
Grogu-e <85890933+Grogu-e@users.noreply.github.com>
2022-12-01 09:47:59 -0500

    final version
    added picture and change font color.

---
+++ TableChair_GroupProject_11-30-22/src/tablechair_groupproject/TableChair_GroupProject.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablechair_groupproject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author strotheb6455
+ */
+public class TableChair_GroupProject
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        new OrderForm().setVisible(true);
+    }   
+     
+    
+}
